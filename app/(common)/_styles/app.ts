b01dadef88cb5bc866e3e2da26d1c9273b2e22e9
阿部良輔 <rabe1020.biz@gmail.com>
2024-03-12 22:19:29 +0900

    add, style constant

--- app/(common)/_styles/app.ts
+++ app/(common)/_styles/app.ts
@@ -5,9 +5,11 @@ const APP_STYLES = {
     secondary: '#CF3F3D',
     textPrimary: 'rgba(0,0,0,0.87)',
     textSecondary: 'rgba(0,0,0,0.54)',
-    hoverPrimary: '#3C3B4A',
-    hoverYellow: 'rgba(253, 226, 71, 0.54)',
     background: '#FAFAFA',
+    hoverYellow: 'rgba(253, 226, 71, 0.54)',
+    hoverPrimary: '#3C3B4A',
+    hoverBackground: '#F0F0F0',
+    errorMain: '#F44336',
   },
   size: {
     headerHeight: 64,
